67e6e0a1e36ecae9db3b581a80c8678aa91b26d5
kukajp3 <kukajp@gmail.com>
2020-10-04 15:43:43 -0300

    proagil app

---
+++ ProAgil.API/Controllers/ValuesController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProAgil.API.Data;
+using ProAgil.API.Model;
+
+namespace ProAgil.API.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ValuesController : ControllerBase
+  {
+    public readonly DataContext Context;
+
+    public ValuesController(DataContext context)
+    {
+      Context = context;
+    }
+
+    // GET api/values
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+      try
+      {
+        return Ok(await Context.Eventos.ToListAsync());
+      }
+      catch (System.Exception)
+      {
+        return this.StatusCode(StatusCodes.Status500InternalServerError, "Failed database!");
+      }
+
+    }
+
+    // GET api/values/5
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get(int id)
+    {
+      try
+      {
+        return Ok(await Context.Eventos.FirstOrDefaultAsync(x => x.EventoId == id));
+      }
+      catch (System.Exception)
+      {
+        return this.StatusCode(StatusCodes.Status500InternalServerError, "Failed database!");
+      }
+
+    }
+
+    // POST api/values
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT api/values/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/values/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+  }
+}
